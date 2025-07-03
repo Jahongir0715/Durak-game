4649ce89a3f376753b4b5b4e2a3222896de6d462
Jahongir0715 <ergashevamohichehra804@gmail.com>
2025-07-03 15:31:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const suits = ['♠', '♥', '♦', '♣'];
+const ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+function createDeck() {
+  const deck = [];
+  for (const suit of suits) {
+    for (const rank of ranks) {
+      deck.push({ suit, rank });
+    }
+  }
+  return deck;
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+const deck = createDeck();
+shuffle(deck);
+
+const playerHand = deck.splice(0, 6);
+const botHand = deck.splice(0, 6);
+
+const trumpCard = deck[deck.length - 1];
+const trumpSuit = trumpCard.suit;
+
+console.log('Козырь:', trumpSuit);
+console.log('Рука игрока:', playerHand);
+console.log('Рука бота:', botHand);
+
+function renderGame() {
+  const gameContainer = document.getElementById('game-container');
+  gameContainer.innerHTML = `
+    <h2>Козырь: ${trumpCard.rank}${trumpCard.suit}</h2>
+    <h3>Ваша рука:</h3>
+    <div id="player-hand"></div>
+  `;
+
+  renderCards(playerHand, 'player-hand');
+}
+
 function renderCards(hand, containerId) {
   const container = document.getElementById(containerId);
   container.innerHTML = '';
