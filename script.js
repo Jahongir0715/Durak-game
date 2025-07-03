42e4b9c61942ceda20cd3db5c32086aad5cb25a9
Jahongir0715 <ergashevamohichehra804@gmail.com>
2025-07-03 15:28:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.getElementById('game-container').innerHTML = '<h2>Добро пожаловать в Durak!</h2><button onclick="alert(\'Пока это просто шаблон\')">Играть</button>';
+document.getElementById('game-container').innerHTML = '<h2>Добро пожаловать в Durak!</h2><button onclick="alert(\'Пока это просто шаблон\')">Играть</button>';renderGame();
